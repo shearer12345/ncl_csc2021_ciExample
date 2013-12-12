1b46e4cfd10d8870f05440bd02281f603172a95e
John Shearer <shearer12345@gmail.com>
2013-12-12 00:25:54 +0000

    add sample rpnCalculator test and production code

---
+++ src/main/java/demo/rpn/RpnCalculator.java
@@ -0,0 +1,16 @@
+package demo.rpn;
+ 
+public class RpnCalculator {
+    public void digit(int value) {
+    }
+ 
+    public void enter() {
+    }
+ 
+    public void operator(String operatorName) {
+    }
+ 
+    public int xRegister() {
+        return 13;
+    }
+}
---
+++ src/test/java/demo/rpn/RpnCalculatorShould.java
@@ -0,0 +1,17 @@
+package demo.rpn;
+ 
+import static org.junit.Assert.assertEquals;
+ 
+import org.junit.Test;
+ 
+public class RpnCalculatorShould {
+    @Test
+    public void addTwoNumbersCorreclty() {
+        RpnCalculator rpnCalculator = new RpnCalculator();
+        rpnCalculator.digit(4);
+        rpnCalculator.enter();
+        rpnCalculator.digit(9);
+        rpnCalculator.operator("+");
+        assertEquals(13, rpnCalculator.xRegister());
+    }
+}
