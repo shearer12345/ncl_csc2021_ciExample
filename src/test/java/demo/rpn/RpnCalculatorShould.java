4474e20fa058ad5cc513cd31123eeab6aaff874f
shearer12345 <shearer12345@gmail.com>
2014-12-02 13:18:05 +0000

    pointless commit adding a blank line

--- src/test/java/demo/rpn/RpnCalculatorShould.java
+++ src/test/java/demo/rpn/RpnCalculatorShould.java
@@ -12,6 +12,7 @@ public class RpnCalculatorShould {
         rpnCalculator.digit(4);
         rpnCalculator.enter();
         rpnCalculator.digit(9);
+        
         rpnCalculator.operator("+");
         assertEquals(13, rpnCalculator.xRegister());
     }
